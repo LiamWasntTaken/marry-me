d9a2695d99906f26fa8a689239f3ba23d165bf6e
LiamWasntTaken <87754036+LiamWasntTaken@users.noreply.github.com>
2025-04-02 02:28:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-// Make the paper draggable
 const papers = document.querySelectorAll(".paper");
 
 papers.forEach(paper => {
-    paper.addEventListener("mousedown", (event) => {
-        let shiftX = event.clientX - paper.getBoundingClientRect().left;
-        let shiftY = event.clientY - paper.getBoundingClientRect().top;
+    // Function to start dragging
+    function startDrag(event) {
+        event.preventDefault(); // Prevents unwanted behavior
 
-        function moveAt(pageX, pageY) {
-            paper.style.left = pageX - shiftX + "px";
-            paper.style.top = pageY - shiftY + "px";
+        let startX, startY;
+
+        if (event.type === "mousedown") {
+            startX = event.clientX - paper.getBoundingClientRect().left;
+            startY = event.clientY - paper.getBoundingClientRect().top;
+
+            document.addEventListener("mousemove", moveDrag);
+            document.addEventListener("mouseup", stopDrag);
+        } else if (event.type === "touchstart") {
+            let touch = event.touches[0];
+            startX = touch.clientX - paper.getBoundingClientRect().left;
+            startY = touch.clientY - paper.getBoundingClientRect().top;
+
+            document.addEventListener("touchmove", moveDrag);
+            document.addEventListener("touchend", stopDrag);
         }
 
-        function onMouseMove(event) {
-            moveAt(event.pageX, event.pageY);
+        function moveDrag(event) {
+            let pageX, pageY;
+
+            if (event.type === "mousemove") {
+                pageX = event.clientX;
+                pageY = event.clientY;
+            } else if (event.type === "touchmove") {
+                let touch = event.touches[0];
+                pageX = touch.clientX;
+                pageY = touch.clientY;
+            }
+
+            paper.style.position = "absolute";
+            paper.style.left = `${pageX - startX}px`;
+            paper.style.top = `${pageY - startY}px`;
         }
 
-        document.addEventListener("mousemove", onMouseMove);
+        function stopDrag() {
+            document.removeEventListener("mousemove", moveDrag);
+            document.removeEventListener("mouseup", stopDrag);
+            document.removeEventListener("touchmove", moveDrag);
+            document.removeEventListener("touchend", stopDrag);
+        }
+    }
 
-        paper.addEventListener("mouseup", () => {
-            document.removeEventListener("mousemove", onMouseMove);
-        });
-    });
-
-    paper.addEventListener("dragstart", () => false);
+    // Attach events for both mouse and touch
+    paper.addEventListener("mousedown", startDrag);
+    paper.addEventListener("touchstart", startDrag);
 });
